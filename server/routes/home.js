26783acb58a4b8dd0f8098edc0835ed999f85115
Abraham Mookhoek <abraham.mookhoek@mavs.uta.edu>
2024-01-10 18:48:42 -0600

    new repo

--- server/routes/home.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import router from './signup';
-router = express.Router();
-
-router.get("/", (req, res) => {
-    res.send("WELCOME TO THE HOMEPAGE")
-})
-
-export default router
