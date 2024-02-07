4d8fa9153fcbbd91ac861f641f443258b2090b1d
Arzenith <patrick.arzou@gmail.com>
2024-02-07 15:56:16 -0600

    temp push

--- app/components/ListeningHistory.tsx
+++ app/components/ListeningHistory.tsx
@@ -6,27 +6,52 @@ import { Add } from "@mui/icons-material";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
+import TrackCard from "./TrackCard";
 
 export default function () {
   return (
-    <div className="padding-container relative z-30 flex h-full w-1/5 flex-col justify-between outline">
+    <div className="padding-container relative z-30 flex w-1/5 flex-col outline">
       <div className="flex w-full justify-center py-2">
         <h2 className="text-2xl">Listening History</h2>
       </div>
-      <div className="flex flex-col">
+      <div
+        style={{ height: "85%" }}
+        className=" flex flex-col overflow-auto outline"
+      >
         <List>
           <ListItem>
-            <ListItemText primary="Track 1 2 3 4" />
+            <TrackCard />
           </ListItem>
           <ListItem>
-            <ListItemText primary="Track 1 2 3 4" />
+            <TrackCard />
           </ListItem>
           <ListItem>
-            <ListItemText primary="Track 1 2 3 4" />
+            <TrackCard />
           </ListItem>
+          <ListItem>
+            <TrackCard />
+          </ListItem>
+          <ListItem>
+            <TrackCard />
+          </ListItem>
+          <ListItem>
+            <TrackCard />
+          </ListItem>
+          <ListItem>
+            <TrackCard />
+          </ListItem>
+          <ListItem>
+            <TrackCard />
+          </ListItem>
+          <ListItem>
+            <TrackCard />
+          </ListItem>
+          {/* <ListItem>
+              <TrackCard />
+            </ListItem> */}
         </List>
       </div>
-      <div className="flex w-full justify-center py-2">
+      <div className="flex-end flex w-full justify-center py-2">
         <Button variant="contained" startIcon={<Add />} color="success">
           New Journal Entry
         </Button>
