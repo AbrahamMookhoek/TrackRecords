00f55945aea9e89677700ed34fa4547d270036af
Arzenith <patrick.arzou@gmail.com>
2024-02-08 15:00:48 -0600

    fixed issues with changing main page layout to grid

--- app/components/ListeningHistory.tsx
+++ app/components/ListeningHistory.tsx
@@ -10,14 +10,11 @@ import TrackCard from "./TrackCard";
 
 export default function () {
   return (
-    <div className="col-span-2 ml-32 flex flex-col outline">
+    <div className="col-span-2 ml-32 flex flex-col overflow-auto outline">
       <div className="flex w-full justify-center py-2">
         <h2 className="text-2xl">Listening History</h2>
       </div>
-      <div
-        style={{ height: "90%", maxHeight: "90%" }}
-        className="flex max-h-min flex-col overflow-auto outline"
-      >
+      <div className="flex max-h-min flex-col overflow-auto ">
         <List>
           <ListItem>
             <TrackCard />
