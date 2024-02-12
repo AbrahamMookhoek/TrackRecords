d30319adfdff9632506576081ce86f96cfef8e64
Abraham Mookhoek <abraham.mookhoek@mavs.uta.edu>
2024-02-11 22:38:20 -0600

    read/write saved tracks on firebase

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,9 +1,22 @@
 import Calendar from "../components/Calendar";
 import Footer from "@/app/components/Footer";
 import Navbar from "@/app/components/Navbar";
-import ListeningHistory from "../components/ListeningHistory";
+
+import { getServerSession } from "next-auth";
+import { options } from "@/app/api/auth/[...nextauth]/options";
+import { spotifyGetSavedTracks } from "@/app/utils/spotify";
+
+import ListeningHistory from "@/app/components/ListeningHistory";
+import { readFromFirestore } from "@/app/firebase/firebase";
 
 export default async function Dashboard() {
+  const session = await getServerSession(options);
+  
+  if (session) {
+    await spotifyGetSavedTracks(session.user.spotify_access_token, session.user?.name);
+    var tracks = await readFromFirestore(session.user?.name);
+  }
+
   return (
     <div className="flex h-screen flex-col justify-between">
       <Navbar />
--- app/shared_objects/Track.js
+++ app/shared_objects/Track.js
@@ -1,5 +1,6 @@
 export class Track {
-    constructor(added_at, album_image, album_name, artist_names, artist_url, track_duration, track_link, track_name) {
+    constructor(spotify_id, added_at, album_image, album_name, artist_names, artist_url, track_duration, track_link, track_name) {
+        this.spotify_id = spotify_id;
         this.added_at = added_at;
         this.album_image = album_image;
         this.album_name = album_name;
@@ -10,4 +11,36 @@ export class Track {
         this.track_name = track_name;
     }
 }
+
+// Firestore data converter
+const trackConverter = {
+    toFirestore: (track) => {
+        return {
+          spotify_id: track.spotify_id,
+          added_at: track.added_at,
+          album_image: track.album_image,
+          album_name: track.album_name,
+          artist_names: track.artist_names,
+          artist_url: track.artist_url,
+          track_duration: track.track_duration,
+          track_link: track.track_link,
+          track_name: track.track_name,
+        };
+    },
+    fromFirestore: (snapshot, options) => {
+        const data = snapshot.data(options);
+        return new Track(
+          data.spotify_id,
+          data.added_at,
+          data.album_image,
+          data.album_name,
+          data.artist_names,
+          data.artist_url,
+          data.track_duration,
+          data.track_link,
+          data.track_name
+        );
+    }
+};
+
 let track = new Track(null, null, null, null, null, null, null, null);
