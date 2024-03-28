856f1eb4be644a45ee087be42aa4cf213ba6a0e3
Gustavo <chavezgustavo814@gmail.com>
2024-03-28 10:28:07 -0500

    added in new layout changes to branch

--- app/dashboard/journal/page.jsx
+++ app/dashboard/journal/page.jsx
@@ -1,15 +1,25 @@
-/*import Image from 'next/image'
+import Footer from "@/app/components/Footer";
+import Navbar from "@/app/components/Navbar";
 
-export default function JournalPage() {
-  return (
-  )
-}*/
-
-import TextEditor from "@/app/components/TextEditor";
 import { getServerSession } from "next-auth";
 import { options } from "@/app/api/auth/[...nextauth]/options";
+import TextEditor from "@/app/components/TextEditor";
+import ListeningHistory from "@/app/components/ListeningHistory";
 
 export default async function JournalPage() {
   const session = await getServerSession(options);
-  return <TextEditor user={session.user} />;
+  return (
+    <div className="grid h-full max-h-screen min-h-screen grid-cols-7 grid-rows-12 gap-y-2 bg-navy-100">
+      <Navbar
+        className={
+          "flex-between col-span-full row-span-1 flex w-full justify-between px-32"
+        }
+      />
+      <div className="col-span-full row-span-10 grid grid-cols-subgrid gap-x-2">
+        <ListeningHistory /> {/*This is what you would need to implement for the sidebar */}
+        <TextEditor user={session.user} />
+      </div>
+      <Footer className={"flexCenter col-span-full row-span-1 w-full px-32"} />
+    </div>
+  );
 }
